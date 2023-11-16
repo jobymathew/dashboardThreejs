a0697c7212688286aee8c4d9c4b74c3c0e2d2565
jobymathew <jobymathew.mec@gmail.com>
2023-11-16 22:30:02 +0800

    add raw load

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
 
  // Add basePath
   basePath: '/dashboardThreejs',
+  webpack: (config, { isServer }) => {
+    // Add raw-loader for GLTF files
+    config.module.rules.push({
+      test: /\.(gltf)$/,
+      use: isServer ? 'raw-loader' : ['raw-loader'],
+    });
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
